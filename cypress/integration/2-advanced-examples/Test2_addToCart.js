a9575595f241c114230a1b971f027c6bbeb36891
philpotti <imhaslop@hotmail.com>
2022-01-31 18:33:21 -0300

    CypressAutomation v2.0

--- cypress/integration/2-advanced-examples/Test2_addToCart.js
+++ cypress/integration/2-advanced-examples/Test2_addToCart.js
@@ -1,20 +1,15 @@
 /// <reference types="Cypress" />
 
-describe('My First Test Suite', function() {
+describe('My Second Test Suite', function() {
 
     it('My First Test Case', function() {
 
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get('.search-keyword').type('ca');
     cy.wait(2000)
-    cy.get('.product').should('have.length',5)
-    cy.get('.product:visible').should('have.length',4)
+  
     //Parent child chainings should
     cy.get('.products').as('productLocator')  // declaring .products as a variable
-    cy.get('@productLocator').find('.product').should('have.length',4)
-    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function(){
-        console.log('sf')
-    })
 
     cy.get('@productLocator').find('.product').each(($el, index, $list) => {
     const textVeg = $el.find('h4.product-name').text()
@@ -22,8 +17,9 @@ describe('My First Test Suite', function() {
     {
         cy.wrap($el).find('button').click()
     }
-
 })
-   
+    cy.get('.cart-icon > img').click()
+    cy.contains('PROCEED TO CHECKOUT').click()
+    cy.contains('Place Order').click()
     })
 })
