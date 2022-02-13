df1e8b5ba162c4bb0af7671dcf86d93f19c61b34
philpotti <89038510+philpotti@users.noreply.github.com>
2022-02-13 18:54:44 -0300

    13-02-2022 Changes

--- cypress/integration/2-advanced-examples/9-SwitchToDifferentTAB_ContinueExecution.js
+++ cypress/integration/2-advanced-examples/9-SwitchToDifferentTAB_ContinueExecution.js
@@ -2,8 +2,17 @@
 
 describe("My 9th Test Suite", function () {
   it("My 9th Test Case", function () {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.visit("https://qaclickacademy.com/practice.php");
 
     cy.get('#opentab').first().invoke('removeAttr','target').click();
     });
 });
+
+/* If NOT TARGET attribute 
+
+cy.get(<locator>).invoke('href')
+.then(link=>{
+cy.visit(link)
+ 
+})
+*/
