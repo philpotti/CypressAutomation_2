92580a28a1a6f53996268951f92421b100f0a673
philpotti <89038510+philpotti@users.noreply.github.com>
2022-03-26 12:55:27 -0300

    03-26-2022 Changes v1.7

--- cypress/integration/2-advanced-examples/I-SwitchToDifferentTAB_ContinueExecution.js
+++ cypress/integration/2-advanced-examples/I-SwitchToDifferentTAB_ContinueExecution.js
@@ -2,12 +2,12 @@
 
 describe('My 9th Test Suite', function () {
 	it('My 9th Test Case', function () {
-		cy.visit('https://qaclickacademy.com/practice.php');
+		cy.visit('http://qaclickacademy.com/practice.php');
 
 		//Remove attribute on a link that opens another tab to open the link on the same tab
 		cy.get('#opentab').invoke('removeAttr', 'target').click();
 
-		cy.url().should('include', 'rahulshettyacademy'); // If it includes 'html str'
+		cy.url().should('include', 'rahulshettyacademy'); // If it includes 'part of html str'
 		cy.url().should('eq', 'https://www.rahulshettyacademy.com/#/index'); // If it is exactly equal to 'html str'
 
 		cy.go('back'); //Go back to the previous page
