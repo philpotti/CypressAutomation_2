d90b4d7ef3962c99df5e714c908368c3331f786e
philpotti <89038510+philpotti@users.noreply.github.com>
2022-03-27 18:51:09 -0300

    03-27-2022 Changes v2.0

--- cypress/integration/2-advanced-examples/L-Handling HTML-Frames.js
+++ cypress/integration/2-advanced-examples/L-Handling HTML-Frames.js
@@ -7,7 +7,7 @@ describe('Frames Test', function () {
 		cy.visit(Cypress.env('url') + '/AutomationPractice/');
 		cy.frameLoaded('#courses-iframe');
 
-		cy.iframe().get("a[href*='mentorship']").eq(0).click();
+		cy.iframe().find("a[href*='mentorship']").eq(0).click();
 
 		//cy.iframe().find('.pricing-title.text-white.ls-1').should('have.length', 2);
 	});
